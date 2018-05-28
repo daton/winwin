fddbfa2d9c629c9ebf0be8f11fd6588034131dc9
Juan Carlos Campos <rapidclimate@gmail.com>
2018-05-28 03:33:08 -0500

    yo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to winwin!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to geer-registro-alumnos!');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { BreadcrumpsComponent } from './shared/breadcrumps/breadcrumps.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PagenotfoundComponent,
+    DashboardComponent,
+    Graficas1Component,
+    PerfilComponent,
+    HeaderComponent,
+
+    NavbarComponent,
+    BreadcrumpsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
